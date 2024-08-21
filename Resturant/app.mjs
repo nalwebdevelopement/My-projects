f1eac4b1feaf5b00e5208a686d65b2e0f6134545
nalwebdevelopement <naliniu91@gmail.com>
2024-08-20 22:58:03 -0400

    Module

--- Resturant/app.mjs
+++ Resturant/app.mjs
@@ -6,7 +6,11 @@ import {t1} from './Table.mjs';
 document.addEventListener('DOMContentLoaded', () => {
    // displayMenu();            // Load and display the menu items
     setupReservationButton(); // Setup reservation button event listener
-    displayContactInfo();      // Display contact information
+    displayContactInfo();  
+
+    console.log(` The table is reserved for  ${t1.name} and has ${t1.noofpersons}`) ;   // Display contact information
+    document.getElementById('reser').textContent = ` The table is reserved for  ${t1.name} and has ${t1.noofpersons} persons`;
+
 });
 
 // document.getElementById("menus").addEventListener('click', () =>
